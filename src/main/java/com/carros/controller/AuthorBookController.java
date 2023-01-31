4388373bba14e5b606c7ee3118af84b9e3e5c8ed
Daniel Santiago <daniel.santiagoti@gmail.com>
2023-01-31 17:55:37 -0300

    adicionando biblioteca de log

--- src/main/java/com/carros/controller/AuthorBookController.java
+++ src/main/java/com/carros/controller/AuthorBookController.java
@@ -4,6 +4,9 @@ import com.carros.dto.AuthorBookDTO;
 import com.carros.model.Author;
 import com.carros.model.Book;
 import com.googlecode.jmapper.JMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +14,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AuthorBookController {
-
+    private static final Logger logger = LogManager.getLogger(AuthorBookController.class);
     @ResponseBody
     @RequestMapping("addAuthorBook")
     public String addAuthorBook(@RequestBody AuthorBookDTO authorBookDTO) {
-
+        logger.info("Esta no adicionando o autor");
         JMapper<Author, AuthorBookDTO> authorMapper = new JMapper<>(Author.class, AuthorBookDTO.class);
 
         Author author = authorMapper.getDestination(authorBookDTO);
